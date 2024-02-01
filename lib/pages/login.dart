0f042336e68758f0950f138738f5683982425357
aswin-mk <aswinmadathumkandy@gmail.com>
2024-02-01 23:46:15 +0530

    hello

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -16,157 +16,157 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF6FB9A9),
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const CircleAvatar(
-                radius: 40.0,
-                backgroundColor: Colors.black,
-                child: Text(
-                  'TR',
+        backgroundColor: const Color(0xFF6FB9A9),
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const CircleAvatar(
+                  radius: 40.0,
+                  backgroundColor: Colors.black,
+                  child: Text(
+                    'TR',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 8.0),
+                const Text(
+                  'Explore the world with a smile',
                   style: TextStyle(
                     color: Colors.white,
-                    fontSize: 32.0,
-                    fontWeight: FontWeight.bold,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.w300,
                   ),
                 ),
-              ),
-              const SizedBox(height: 8.0),
-              const Text(
-                'Explore the world with a smile',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 16.0,
-                  fontWeight: FontWeight.w300,
-                ),
-              ),
-              const SizedBox(height: 32.0),
-              const Text(
-                'Hi, nice to see you again',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-              const SizedBox(height: 16.0),
-              TextField(
-                controller: _emailController,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                  labelText: 'Email',
-                  labelStyle: TextStyle(color: Colors.white),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                ),
-                style: const TextStyle(color: Colors.white),
-              ),
-              const SizedBox(height: 16.0),
-              TextField(
-                controller: _passwordController,
-                obscureText: true,
-                decoration: const InputDecoration(
-                  labelText: 'Password',
-                  labelStyle: TextStyle(color: Colors.white),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white),
-                  ),
-                ),
-                style: const TextStyle(color: Colors.white),
-              ),
-              const SizedBox(height: 16.0),
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {
-                    // TODO: implement forgot password logic
-                  },
-                  child: const Text(
-                    'Forgot password?',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: () {
-                  signin(context,_emailController.text, _passwordController.text);
-                },
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: const Color(0xFFF5E9C9),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
-                ),
-                child: const Text('Sign in'),
-              ),
-              const SizedBox(height: 16.0),
-              const Text(
-                'Or sign in with',
-                style: TextStyle(color: Colors.white),
-              ),
-              const SizedBox(height: 16.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  IconButton(
-                    onPressed: () {
-                      // TODO: implement sign in with Google
-                    },
-                    icon: const Icon(Icons.g_mobiledata),
+                const SizedBox(height: 32.0),
+                const Text(
+                  'Hi, nice to see you again',
+                  style: TextStyle(
                     color: Colors.white,
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w500,
                   ),
-                  IconButton(
+                ),
+                const SizedBox(height: 16.0),
+                TextField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: const InputDecoration(
+                    labelText: 'Email',
+                    labelStyle: TextStyle(color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  style: const TextStyle(color: Colors.white),
+                ),
+                const SizedBox(height: 16.0),
+                TextField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    labelStyle: TextStyle(color: Colors.white),
+                    enabledBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                    ),
+                  ),
+                  style: const TextStyle(color: Colors.white),
+                ),
+                const SizedBox(height: 16.0),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
                     onPressed: () {
-                      // TODO: implement sign in with Apple
-                    },
-                    icon: const Icon(Icons.apple),
-                    color: Colors.white,
-                  ),
-                  IconButton(
-                    onPressed: () {
-                      // TODO: implement sign in with Facebook
-                    },
-                    icon: const Icon(Icons.facebook),
-                    color: Colors.white,
-                  ),
-                ],
-              ),
-              const SizedBox(height: 16.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    'Don\'t have an account?',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      // TODO: implement sign up logic
-                      Navigator.pushNamed(context, '/signup');
+                      // TODO: implement forgot password logic
                     },
                     child: const Text(
-                      'Sign up',
+                      'Forgot password?',
                       style: TextStyle(color: Colors.white),
                     ),
                   ),
-                ],
-              ),
-            ],
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {
+                    signin(context,_emailController.text, _passwordController.text);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFF5E9C9),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                  child: const Text('Sign in'),
+                ),
+                const SizedBox(height: 16.0),
+                const Text(
+                  'Or sign in with',
+                  style: TextStyle(color: Colors.white),
+                ),
+                const SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                      onPressed: () {
+                        // TODO: implement sign in with Google
+                      },
+                      icon: const Icon(Icons.g_mobiledata),
+                      color: Colors.white,
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        // TODO: implement sign in with Apple
+                      },
+                      icon: const Icon(Icons.apple),
+                      color: Colors.white,
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        // TODO: implement sign in with Facebook
+                      },
+                      icon: const Icon(Icons.facebook),
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Don\'t have an account?',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        // TODO: implement sign up logic
+                        Navigator.pushNamed(context, '/signup');
+                      },
+                      child: const Text(
+                        'Sign up',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
   }
 }
--- lib/services/signin.dart
+++ lib/services/signin.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -6,13 +8,26 @@ final FirebaseAuth _auth = FirebaseAuth.instance;
 
 void signin(BuildContext context, String email, String password) async {
   try {
+
+    showDialog(
+      context: context, 
+      builder: (context){
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+    }
+    );
+
     final UserCredential userCredential = await _auth.signInWithEmailAndPassword(
       email: email,
       password: password,
     );
 
+
+
     // Successful sign-in
     User? user = userCredential.user;
+    Navigator.pop(context);
     if (user != null) {
       // Navigate to home screen or other relevant screen
       Navigator.pushNamed(context, '/homepage');
@@ -28,3 +43,4 @@ void signin(BuildContext context, String email, String password) async {
     }
   }
 }
+
