85ca1c4a3210d15b32c6a21fa71c26c66c08b0d2
aswin-mk <aswinmadathumkandy@gmail.com>
2024-02-19 11:36:46 +0530

    hiughsfodu

---
+++ lib/services/showalert.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void ShowAlert(BuildContext context,String Message){
+  showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text("Alert"),
+        content: Text(Message),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text("Close"),
+          ),
+        ],
+      );
+    },
+  );
+}
